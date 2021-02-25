6f9d67a4ba0fe9d2a753ec7e8c432d9cd91e9de4
MarkovOleg <markov-oa@mail.ru>
2021-02-25 07:55:19 +0500

    Refactored export_to_csv. It is works, but i want to better

--- todo_api/todo/utils.py
+++ todo_api/todo/utils.py
@@ -1,44 +1,30 @@
 import csv
 
-from django.http import HttpResponse
+from .models import Todo
 
 
-def export_to_csv(queryset, fields, titles, file_name):
+def export_to_csv(file_name, response):
     """
     Export the model data in the form of csv file
-    :param queryset: queryset that need to be exported as csv
-    :param fields: fields of a model that will be included in csv
-    :param titles: title for each cell of the csv record
+    :param response:
     :param file_name: the exported csv file name
+
     """
-    model = queryset.model
-    response = HttpResponse(content_type='text/csv')
-    # force download
-    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(file_name)
-    # the csv writer
+    if '.csv' not in file_name:
+        file_name += '.csv'
+        response['Content-Disposition'] = f'attachment; filename={file_name}'
+
+    queryset = Todo.objects.only('task', 'timestamp', 'completed', 'updated', 'finish_date', 'user')
+    fields = ['task', 'timestamp', 'completed', 'updated', 'finish_date', 'user']
     writer = csv.writer(response)
-    if fields:
-        headers = fields
-        if titles:
-            titles = titles
-        else:
-            titles = headers
-    else:
-        headers = []
-        for field in model._meta.fields:
-            headers.append(field.name)
-        titles = headers
-
-    # Writes the title for the file
-    writer.writerow(titles)
-
-    # write data rows
+    writer.writerow(fields)
     for item in queryset:
-        writer.writerow([getattr(item, field) for field in headers])
+        writer.writerow([getattr(item, field) for field in fields])
+
     return response
 
 
-def import_to_db_from_csv(FilePath, Todo):
+def import_to_db_from_csv(FilePath):
     with open(FilePath, "r") as file:
         file_reader = csv.DictReader(file, delimiter=",")
         for line_dict in file_reader:
